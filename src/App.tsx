ea8e8bcbce03e65e68bb6ef545d62a6ec680c4ca
= <harmostparmod@gmail.com>
2024-04-27 18:16:55 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-[#202020]">
+        <h1 className="text-4xl text-center text-white  ">Make What u feel </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
